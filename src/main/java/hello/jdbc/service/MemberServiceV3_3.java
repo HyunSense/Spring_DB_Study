6c55b5a747c4b8721f71254e4dd28cc317a1e715
Hyunsense <hyunsense1022@gmail.com>
2023-07-17 11:22:22 +0900

    Feat. Java Exception 추가

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -6,35 +6,25 @@ import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랙잭션 매니저
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_3 {
 
-    private final PlatformTransactionManager transactionManager;
     private final MemberRepositoryV3 memberRepository;
 
-    public void accountTransfer(String fromId, String toId, int money){
-        //트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
-        try {
-            //비즈니스 로직
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); //성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status); //실패시 롤백, 트랜잭션매니저가 알아서 릴리즈(con..)을 닫아준다.
-            throw new IllegalStateException(e);
-        }
-
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
